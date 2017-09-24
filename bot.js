03491c6bf3dc52309c33ab2e0b017b1c6345c8e6
jritch <jacob.w.k.ritchie@gmail.com>
2017-09-24 16:09:25 -0400

    Added suggestion

--- bot.js
+++ bot.js
@@ -6,13 +6,16 @@ var T = new twit(require('./config.js'));
 story = {
   "noun" : ["shoe","boot","corset","wand"],
   "verb" : ["fits","aches","falls deeply in love"],
+  "active-verb" : ["fix","break","plug","promote","whack"],
+  "adjective" : ["broke","poor","leaky","underfed"],
   "animal" : ["bird"],
   "if-shoe" : ["If the #noun# #verb#..."],
   "early-bird" : ["The early #animal# gets the #noun#."],
   "two-in" : ["#number# in the #noun# are worth #number# in the #noun#"],
   "get-two" : ["Get #number# #noun#s stoned at once."],
   "number": ["1","2","3","69"],
-	"origin": ["#if-shoe#","#early-bird#","#two-in#","#get-two#"]
+  "broke" : ["If it ain't #adjective#, don't #active-verb# it."],
+  "origin": ["#if-shoe#","#early-bird#","#two-in#","#get-two#","#broke#"]
     }
 
 grammar = tracery.createGrammar(story);
@@ -21,13 +24,11 @@ console.log(aphorism);
 
 T.post('statuses/update', {status: aphorism}, function (error, data, response) {
   if (response) {
-    console.log('Tweeted!');
-    //console.log(data)
-    //console.log(error)
-    console.log(error)
-  }
-  else {
-    console.log("Error!");
-    console.log(error);
+    if (error){
+      console.log(error)
+    }
+    else {
+      console.log('Tweeted!');
+    }
   }
 });
